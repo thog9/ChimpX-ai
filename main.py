f1b1fa3e137b33fe4d3a29d4241fd71249d73530
Thog <87602429+thog9@users.noreply.github.com>
2026-05-28 22:54:34 +0700

    !!!

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def _banner():
     print(f"{Fore.GREEN}{banner:^80}{Style.RESET_ALL}")
     print(f"{Fore.GREEN}{'═' * BORDER_WIDTH}{Style.RESET_ALL}")
     print_border("CHIMPX AI", Fore.GREEN)
-    print(f"{Fore.YELLOW}│ {'Website'}: {Fore.CYAN}https://app.chimpx.ai/{Style.RESET_ALL}")
+    print(f"{Fore.YELLOW}│ {'Website'}: {Fore.CYAN}https://thogtoolhub.com/{Style.RESET_ALL}")
     print(f"{Fore.YELLOW}│ {'Discord'}: {Fore.CYAN}https://discord.gg/MnmYBKfHQf{Style.RESET_ALL}")
     print(f"{Fore.YELLOW}│ {'Channel Telegram'}: {Fore.CYAN}https://t.me/thogairdrops{Style.RESET_ALL}")
     print(f"{Fore.GREEN}{'═' * BORDER_WIDTH}{Style.RESET_ALL}")
